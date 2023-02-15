0430477896728e9cf70b06f354c37032e169c98c
PabloMateosP <pmatpal0105@g.educaand.es>
2023-02-15 10:27:24 +0100

    subida ejemplos

--- src/Ejemplo_clase/EjemploLista.java
+++ src/Ejemplo_clase/EjemploLista.java
@@ -8,8 +8,17 @@ public class EjemploLista {
         cars.add("volvo");
         cars.add("BMW");
         cars.add("Seat");
-        cars.add("Volkswagen");
-        cars.add("Renault");
+        cars.add("Fockswagen");
+        cars.add("Toyora");
+        System.out.println(cars);
+
+        System.out.println(cars.get(0));
+
+        cars.remove("BMW");
+        cars.add(2, "Bugatata");
+        System.out.println(cars);
+
+        cars.add(3, "Lamberjumber");
         System.out.println(cars);
 
         System.out.println(cars.get(2));
@@ -21,10 +30,18 @@ public class EjemploLista {
         System.out.println(" ");
 
         System.out.println(cars.remove("Seat")); //Nos dirá true o false dependiendo de si ha podido completar o no.
+
         cars.remove("Seat");
         System.out.println(cars);
 
-        cars.set(2, "Ferrari"); //Con el set nos cambia lo que se encuentra en el número de ese indice por el string que introduzcamos
+        cars.set(2, "Ferrirri"); //Con el set nos cambia lo que se encuentra en el número de ese indice por el string que introduzcamos
         System.out.println(cars);
+
+        cars.removeAll(cars);
+        System.out.println(cars);
+        System.out.println(cars.toArray().length);
+
+        ArrayList<String> cars2 = new ArrayList<>();
+
     }
 }
