e2ae9d4d5581e04141e1e25fc54d46f45e395795
PabloMateosP <pmatpal0105@g.educaand.es>
2023-02-22 17:49:23 +0100

    Subida 5.1

--- src/Actividades/Actividad5_1.java
+++ src/Actividades/Actividad5_1.java
@@ -7,10 +7,10 @@ import java.util.TreeSet;
 public class Actividad5_1 {
     public static void main(String[] args) {
         TreeSet<Persona> Personas = new TreeSet<>();
-        Persona Manolo = new Persona();
-        Persona Adri = new Persona();
-        Persona Pepa = new Persona();
-        Persona Ricky = new Persona();
+        Persona Manolo = new Persona(24, "Manolo");
+        Persona Adri = new Persona(31, "Adri");
+        Persona Pepa = new Persona(14, "Pepa");
+        Persona Ricky = new Persona(18, "Ricky");
 
         Personas.add(Manolo);
         Personas.add(Ricky);
